3b08544d29556805a856a131269f546ab157cbe1
Sara Nolen <nolensara1@gmail.com>
2020-11-12 23:14:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 def position_taken?(board, index)
 if board[index] == " " || board[index] == "" || board[index] == nil
   return false
-end
-if board[index] == "X" || board[index] == "O"
+
+elsif board[index] == "X" || board[index] == "O"
   return true
 end
 end
